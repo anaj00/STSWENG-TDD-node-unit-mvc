d98c26556ebe94ccb0d8056c2968f040d76d9af7
ComfyJace <jason_erwin_dimalanta@dlsu.edu.ph>
2024-02-05 23:57:44 +0800

    feat: findPost test cases

--- tests/post.test.js
+++ tests/post.test.js
@@ -194,5 +194,83 @@ describe('Post controller', () => {
 
     describe('findPost', () => {
 
+        var findPostStub;
+
+        beforeEach(() => {
+            // before every test case setup first
+            res = {
+                json: sinon.spy(),
+                status: sinon.stub().returns({ end: sinon.spy() })
+            };
+        });
+
+        afterEach(() => {
+            // executed after the test case
+            findPostStub.restore();
+        });
+
+        it('should return post once found', () =>{
+            expectedResult = {
+                _id: '507asdghajsdhjgasd',
+                title: 'My first test post',
+                content: 'Random content',
+                author: 'stswenguser',
+                date: Date.now()
+            };
+
+            findPostStub = sinon.stub(PostModel, 'findPost').yields(null, expectedResult);
+
+            // Act
+            PostController.findPost(req, res);
+
+            // Assert
+            sinon.assert.calledWith(PostModel.findPost, req.body);
+            sinon.assert.calledWith(res.json, sinon.match({ title: req.body.title }));
+            sinon.assert.calledWith(res.json, sinon.match({ content: req.body.content }));
+            sinon.assert.calledWith(res.json, sinon.match({ author: req.body.author }));
+        })
+
+        // Error scenario: Post not found
+        it('should handle post not found', () => {
+            let req = {
+                //TODO: change if poses error
+                body: {
+                    _id: 'ilovesoup',
+                    title: 'testing is 4 nerdz',
+                    content: 'drop a db',
+                    author: 'sejungpark',
+                }
+            };
+
+            // Act
+            PostController.findPost(req, res);
+
+            // Assert
+            sinon.assert.calledWith(PostModel.findPost, req.body);
+            sinon.assert.calledWith(res.status, 404); 
+            sinon.assert.calledWith(res.json, sinon.match({ error: 'Post not found' }));
+        })
+        
+        // Error Scenario: Uncatched Error
+        
+        it('should return status 500 on server error', () => {
+            let req = {
+                //TODO: change if poses error
+                body: {
+                    _id: 'ilovesoup',
+                    title: 'testing is 4 nerdz',
+                    content: 'drop a db',
+                    author: 'sejungpark',
+                }
+            }
+
+            // Act
+            PostController.findPost(req, res);
+
+            // Assert
+            sinon.assert.calledWith(PostModel.findPost, req.body);
+            sinon.assert.calledWith(res.status, 500); 
+            sinon.assert.calledOnce(res.status(500).end);
+        })
     })
 });
